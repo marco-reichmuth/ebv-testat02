8b22aab8daa1b382bea4993bdead92bfb31b1a2b
oscar <oscar@oscar.(none)>
2013-05-11 15:46:59 +0200

    Testat2 automatischer Schwellwert (Otsu)

--- process_frame.c
+++ process_frame.c
@@ -25,6 +25,16 @@ void ProcessFrame(uint8 *pInputImg) {
 	struct OSC_VIS_REGIONS ImgRegions;//these contain the foreground objects
 
 	//Testat2
+	uint32 Hist[256]; //Histogram
+	uint8 *p = data.u8TempImage[GRAYSCALE]; //Pointer to picture
+	uint32 W0[256]; //Omega0
+	uint32 W1[256]; //Omepa1
+	uint32 M0[256]; //My0
+	uint32 M1[256]; //My1
+	uint32 sb2[256]; //Sigma b im Quadrat
+	short k, g, thr;
+
+	memset(Hist, 0, sizeof(Hist));
 
 	if (data.ipc.state.nStepCounter == 1) {
 		/* this is the first time we call this function */
@@ -36,68 +46,133 @@ void ProcessFrame(uint8 *pInputImg) {
 				sizeof(data.u8TempImage[FGRCOUNTER]));
 	} else {
 		/* this is the default case */
-		// Schwellwert manuell
-		for (r = 0; r < siz; r += nc)/* we strongly rely on the fact that them images have the same size */
+		//Wenn Threshold ungleich 0 dann wird manueller Schwellwert benutzt
+		if (data.ipc.state.nThreshold != 0) {
+			for (r = 0; r < siz; r += nc)/* we strongly rely on the fact that them images have the same size */
+			{
+				for (c = 0; c < nc; c++) {
+					/* first determine the foreground estimate */
+					data.u8TempImage[THRESHOLD][r + c]
+							= (short) data.u8TempImage[GRAYSCALE][r + c]
+									> data.ipc.state.nThreshold ? 0 : 0xff; //Testat2 Manueller Schwellwert auf Bild anwenden
+				}
+			}
+
+			//Wenn Threshold gleich 0 dann wird ein automatischer Schwellwert benutzt
+		} else {
+			// Histogramm füllen
+			for (r = 0; r < siz; r += nc) {
+				for (c = 0; c < nc; c++) {
+					Hist[p[r + c]] += 1;
+				}
+			}
+			//grösstes Sigma (thr) auf 0 setzen
+			thr = 0;
+			//Histogramm auswerten
+			for (k = 0; k <= 255; k++) {
+				//W0 und M0 bestimmen (Zuerst auf 0 setzen)
+				W0[k] = 0;
+				M0[k] = 0;
+				for (g = 0; g <= k; g++) {
+					W0[k] += Hist[g];
+					M0[k] += Hist[g] * g;
+				}
+				//W1 und M1 bestimmen (Zuerst auf 0 setzen)
+				W1[k] = 0;
+				M1[k] = 0;
+				for (g = k + 1; g <= 255; g++) {
+					W1[k] += Hist[g];
+					M1[k] += Hist[g] * g;
+				}
+				//sb2 bestimmen
+				sb2[k] = (uint32) (pow(((1 / (double) W0[k]) * (double) M0[k]
+						- (1 / (double) W1[k]) * (double) M1[k]), 2) / 90240
+						* W0[k] * W1[k]);
+				//grösstes Signma bestimmen
+				if (sb2[k] > sb2[thr]) {
+					thr = k;
+				}
+			}
+			//Otsu auf Bild anwenden
+			for (r = 0; r < siz; r += nc)/* we strongly rely on the fact that them images have the same size */
+			{
+				for (c = 0; c < nc; c++) {
+					/* first determine the foreground estimate */
+					data.u8TempImage[THRESHOLD][r + c]
+							= (short) data.u8TempImage[GRAYSCALE][r + c] > thr ? 0
+									: 0xff; //Testat2
+				}
+			}
+			//for debugging -> log Histogram
+			//			for (k=0;k<=255;k++)
+			//			{
+			//				//log all
+			//				OscLog(INFO, "[%d]\t %d\t %d\t %d\t %d\t %d\t %f\n",k,Hist[k],W0[k],M0[k],W1[k],M1[k],ob2[k]);
+			//				//log custom
+			//				OscLog(INFO, "Hist[%d]\t %d\n", k , Hist[k]);
+			//				OscLog(INFO, "W0[%d]\t %d\n", k , W0[k]);
+			//				OscLog(INFO, "M0[%d]\t %d\n", k , M0[k]);
+			//				OscLog(INFO, "W1[%d]\t %d\n", k , W1[k]);
+			//				OscLog(INFO, "M1[%d]\t %d\n", k , M1[k]);
+			//	O			scLog(INFO, "ob2[%d]\t %d\n", k , ob2[k]);
+			//
+			//			}
+			//			OscLog(INFO, "thr\t %d\n", thr);
+
+
+		}
+
+		// Schliessung machen (Dilation & Erosion)
+
+		for (r = nc; r < siz - nc; r += nc)/* we skip the first and last line */
 		{
-			for (c = 0; c < nc; c++) {
-				/* first determine the foreground estimate */
-				data.u8TempImage[THRESHOLD][r + c]
-						= (short) data.u8TempImage[GRAYSCALE][r + c]
-								> data.ipc.state.nThreshold ? 0 : 0xff; //Testat2 Manueller Schwellwert auf Bild anwenden
+			for (c = 1; c < nc - 1; c++)/* we skip the first and last column */
+			{
+				unsigned char* p = &data.u8TempImage[THRESHOLD][r + c];
+				data.u8TempImage[DILATION][r + c] = *(p - nc - 1) | *(p - nc)
+						| *(p - nc + 1) | *(p - 1) | *p | *(p + 1) | *(p + nc
+						- 1) | *(p + nc) | *(p + nc + 1);
+
 			}
 		}
-	}
 
-	// Schliessung machen (Dilation & Erosion)
-
-	for (r = nc; r < siz - nc; r += nc)/* we skip the first and last line */
-	{
-		for (c = 1; c < nc - 1; c++)/* we skip the first and last column */
+		for (r = nc; r < siz - nc; r += nc)/* we skip the first and last line */
 		{
-			unsigned char* p = &data.u8TempImage[THRESHOLD][r + c];
-			data.u8TempImage[DILATION][r + c] = *(p - nc - 1) | *(p - nc) | *(p
-					- nc + 1) | *(p - 1) | *p | *(p + 1) | *(p + nc - 1) | *(p
-					+ nc) | *(p + nc + 1);
-
+			for (c = 1; c < nc - 1; c++)/* we skip the first and last column */
+			{
+				unsigned char* p = &data.u8TempImage[DILATION][r + c];
+				data.u8TempImage[EROSION][r + c] = *(p - nc - 1) & *(p - nc)
+						& *(p - nc + 1) & *(p - 1) & *p & *(p + 1) & *(p + nc
+						- 1) & *(p + nc) & *(p + nc + 1);
+			}
 		}
+
+		// Testat2
+
+
+		//wrap image DILATION in picture struct
+		Pic1.data = data.u8TempImage[DILATION];
+		Pic1.width = nc;
+		Pic1.height = OSC_CAM_MAX_IMAGE_HEIGHT / 2;
+		Pic1.type = OSC_PICTURE_GREYSCALE;
+		//as well as EROSION (will be used as output)
+		Pic2.data = data.u8TempImage[EROSION];
+		Pic2.width = nc;
+		Pic2.height = OSC_CAM_MAX_IMAGE_HEIGHT / 2;
+		Pic2.type = OSC_PICTURE_BINARY;//probably has no consequences
+		//have to convert to OSC_PICTURE_BINARY which has values 0x01 (and not 0xff)
+		OscVisGrey2BW(&Pic1, &Pic2, 0x80, false);
+
+		//now do region labeling and feature extraction
+		OscVisLabelBinary(&Pic2, &ImgRegions);
+		OscVisGetRegionProperties(&ImgRegions);
+
+		//OscLog(INFO, "number of objects %d\n", ImgRegions.noOfObjects);
+		//plot bounding boxes both in gray and dilation image
+		Pic2.data = data.u8TempImage[GRAYSCALE];
+		OscVisDrawBoundingBoxBW(&Pic2, &ImgRegions, 255);
+		OscVisDrawBoundingBoxBW(&Pic1, &ImgRegions, 128);
 	}
-
-	for (r = nc; r < siz - nc; r += nc)/* we skip the first and last line */
-	{
-		for (c = 1; c < nc - 1; c++)/* we skip the first and last column */
-		{
-			unsigned char* p = &data.u8TempImage[DILATION][r + c];
-			data.u8TempImage[EROSION][r + c] = *(p - nc - 1) & *(p - nc) & *(p
-					- nc + 1) & *(p - 1) & *p & *(p + 1) & *(p + nc - 1) & *(p
-					+ nc) & *(p + nc + 1);
-		}
-	}
-
-	// Testat2
-
-
-	//wrap image DILATION in picture struct
-	Pic1.data = data.u8TempImage[DILATION];
-	Pic1.width = nc;
-	Pic1.height = OSC_CAM_MAX_IMAGE_HEIGHT / 2;
-	Pic1.type = OSC_PICTURE_GREYSCALE;
-	//as well as EROSION (will be used as output)
-	Pic2.data = data.u8TempImage[EROSION];
-	Pic2.width = nc;
-	Pic2.height = OSC_CAM_MAX_IMAGE_HEIGHT / 2;
-	Pic2.type = OSC_PICTURE_BINARY;//probably has no consequences
-	//have to convert to OSC_PICTURE_BINARY which has values 0x01 (and not 0xff)
-	OscVisGrey2BW(&Pic1, &Pic2, 0x80, false);
-
-	//now do region labeling and feature extraction
-	OscVisLabelBinary(&Pic2, &ImgRegions);
-	OscVisGetRegionProperties(&ImgRegions);
-
-	//OscLog(INFO, "number of objects %d\n", ImgRegions.noOfObjects);
-	//plot bounding boxes both in gray and dilation image
-	Pic2.data = data.u8TempImage[GRAYSCALE];
-	OscVisDrawBoundingBoxBW(&Pic2, &ImgRegions, 255);
-	OscVisDrawBoundingBoxBW(&Pic1, &ImgRegions, 128);
 }
 
 
